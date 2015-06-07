e923b862861eadaee10b7609ada2ada64b590f53
JLevstein <jjlevstein@gmail.com>
2015-06-07 19:05:31 +0900

    Added sound macros file

---
+++ sound-macros.js
@@ -0,0 +1,98 @@
+macros["playSound"] = {
+  handler: function (bodyElement, macroName, params, parser) {
+    var soundFile, audioElement;
+
+    soundFile = eval(parser.fullArgs()).toString();
+    audioElement = document.createElement('audio');
+
+    audioElement.setAttribute('id', 'audio');
+    audioElement.setAttribute('src', soundFile);
+    audioElement.volume = 1;
+
+    document.body.appendChild(audioElement);
+    document.getElementById('audio').play();
+  }
+}
+
+macros["loopSound"] = {
+  handler: function (bodyElement, macroName, params, parser) {
+    var soundFile, audioElement;
+
+    soundFile = eval(parser.fullArgs()).toString();
+    if(document.querySelectorAll("audio[src='"+soundFile+"']")[0]) {
+      audioElement = document.querySelectorAll("audio[src='"+soundFile+"']")[0];
+    } else {
+      audioElement = document.createElement('audio');
+      audioElement.setAttribute('id', 'audio');
+      audioElement.setAttribute('src', soundFile);
+      audioElement.volume = 1;
+      document.body.appendChild(audioElement);
+    }
+
+    audioElement.play();
+    audioElement.setAttribute('loop', true);
+  }
+}
+
+macros["pauseSound"] = {
+  handler: function (bodyElement, macroName, params, parser) {
+    var soundFile, audioElement;
+
+    soundFile = eval(parser.fullArgs()).toString();
+    audioElement = document.querySelectorAll("audio[src='"+soundFile+"']")[0];
+
+    audioElement.pause();
+  }
+}
+
+macros["increaseVolume"] = {
+  handler: function (bodyElement, macroName, params, parser) {
+    var soundFile, audioElement;
+
+    soundFile = eval(parser.fullArgs()).toString();
+    audioElement = document.querySelectorAll("audio[src='"+soundFile+"']")[0];
+
+    if(audioElement.volume < 0.99) {
+      audioElement.volume += 0.1;
+    } else {
+      return false;
+    }
+  }
+}
+
+macros["decreaseVolume"] = {
+  handler: function (bodyElement, macroName, params, parser) {
+    var soundFile, audioElement;
+
+    soundFile = eval(parser.fullArgs()).toString();
+    audioElement = document.querySelectorAll("audio[src='"+soundFile+"']")[0];
+
+    if(audioElement.volume > 0.1) {
+      audioElement.volume -= 0.1;
+    } else {
+      return false;
+    }
+  }
+}
+
+macros["increasePlayback"] = {
+  handler: function (bodyElement, macroName, params, parser) {
+    var soundFile, audioElement;
+
+    soundFile = eval(parser.fullArgs()).toString();
+    audioElement = document.querySelectorAll("audio[src='"+soundFile+"']")[0];
+
+    audioElement.playbackRate += 0.1;
+  }
+}
+
+macros["decreasePlayback"] = {
+  handler: function (bodyElement, macroName, params, parser) {
+    var soundFile, audioElement;
+
+    soundFile = eval(parser.fullArgs()).toString();
+    audioElement = document.querySelectorAll("audio[src='"+soundFile+"']")[0];
+
+    audioElement.playbackRate -= 0.1;
+  }
+}
